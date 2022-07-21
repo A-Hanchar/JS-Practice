80c32cee4c4b88aa0e2d6570b2c42f4f0d9faaf9
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2022-07-21 18:45:28 +0400

    subtract-the-product-and-sum-of-digits-of-an-integer

--- exercises/subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ exercises/subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -1,15 +1,15 @@
 /**
- * @see
+ * @see https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer
  * @param {number} n
  * @return {number}
  */
 const subtractProductAndSum = n => {
-  let sum = 0;
+  let sum = 0
   let multi = 1
 
   const nAsString = new String(n)
 
-  for(const digit of nAsString) {
+  for (const digit of nAsString) {
     sum += +digit
     multi *= +digit
   }
