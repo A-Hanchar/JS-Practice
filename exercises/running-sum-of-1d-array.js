44e062c12e1def8d782ac063896df840ad1e6af7
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2022-08-08 18:52:03 +0400

    running-sum-of-1d-array

---
+++ exercises/running-sum-of-1d-array.js
@@ -0,0 +1,22 @@
+/**
+ * @see https://leetcode.com/problems/running-sum-of-1d-array/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = nums => {
+  let sum = nums[0]
+
+  const result = [sum]
+
+  for (let i = 1; i < nums.length; i++) {
+    result.push(nums[i] + sum)
+
+    sum += nums[i]
+  }
+
+  return result
+}
+
+console.log(runningSum([1, 2, 3, 4]))
+console.log(runningSum([1, 1, 1, 1, 1]))
+console.log(runningSum([3, 1, 2, 10, 1]))
