5c65fe72dcce26d8d654e89b69b74aa1e8a6e820
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2022-09-15 13:42:43 +0400

    update binary-search

--- exercises/binary-search.js
+++ exercises/binary-search.js
@@ -8,23 +8,44 @@ const search = (nums, target) => {
   let startIndex = 0
   let endIndex = nums.length - 1
 
-  do {
-    if (startIndex === endIndex) return -1
-
+  while (startIndex <= endIndex) {
     const middleIndex = Math.floor((startIndex + endIndex) / 2)
 
     const middleValue = nums[middleIndex]
 
     if (middleValue === target) return middleIndex
 
-    if (middleValue > target) {
-      endIndex = middleIndex
-    }
-
     if (middleValue < target) {
       startIndex = middleIndex + 1
     }
-  } while (true)
+
+    if (middleValue > target) {
+      endIndex = middleIndex - 1
+    }
+  }
+
+  return -1
+  //       let startIndex = 0
+  //   let endIndex = nums.length - 1
+
+  //   do {
+  //     const middleIndex = Math.floor((startIndex + endIndex) / 2)
+
+  //     const middleValue = nums[middleIndex]
+
+  //     if(middleValue === target) return middleIndex
+
+  //     if(startIndex === endIndex) return -1
+
+  //     if(middleValue > target) {
+  //       endIndex = middleIndex
+  //     }
+
+  //     if(middleValue < target ) {
+  //       startIndex = middleIndex + 1
+  //     }
+
+  //   } while (true);
 }
 
 console.log(search([-1, 0, 3, 5, 9, 12], 9))
